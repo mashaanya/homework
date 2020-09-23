7c8b2187a87ee9aa62f7df3b446c78985fbc76aa
Anna Sivohina <38498089+mashaanya@users.noreply.github.com>
2020-09-23 22:31:49 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,25 +1,31 @@
 # homework
 import argparse
-import os
+import os,time
 st = os.getcwd()
-stinfo=os.stat('/Users/admin/PycharmProjects/pythonProject/hf.py')
+stinfo = os.stat('/Users/admin/PycharmProjects/pythonProject/hf.py')
 parser=argparse.ArgumentParser()
-'''
-in command line you should require only '-i (something)' and you will get everything that you want
-or you can input 'python name of the directory' enter
-sorry, i don't understand how to get name of the file i think my code is wrong but it works somehow)) and i don't grasp how to remane, because of don't know the name of the file and i have problems with arguments in command line 
-'''
-parser.add_argument("-i","--information",help='information you want to get')
-parser.add_argument("-f","--filename",default=os.path,nargs=1,help="file name")
-parser.add_argument('-m','--mtime',default=os.path.getmtime(st),nargs=1,help="This is modification time")
-parser.add_argument('-s','--size',default=stinfo.st_size,nargs=1,help='This is the size of the file')
-#parser.add_argument('-r','--rename',default=os.renames(st,input()),help='this rename your file')
+#parser.add_argument("-i","--information",action='store_true',help='information you want to get')
+parser.add_argument("-f","--filename",action='store_true',help="file name")
+parser.add_argument('-m','--mtime',action='store_true',help="This is modification time")
+parser.add_argument('-s','--size',action='store_true',help='This is the size of the file')
+parser.add_argument('-r','--rename',action='store_true',help='this rename your file')
 args=parser.parse_args()
 filename=args.filename
 mtime=args.mtime
 size=args.size
-#rename=args.rename
-print(filename)
-print(mtime)
-print(size)
-#print(rename)
+rename=args.rename
+if mtime:
+
+    ti=time.ctime(stinfo.st_mtime)
+    print('The last modofication time is ', ti)
+if size:
+    s=os.path.getsize('/Users/admin/PycharmProjects/pythonProject/hf.py')
+    print('The size of the file is', s)
+if filename:
+    print('The file is','/Users/admin/PycharmProjects/pythonProject/hf.py')
+if rename:
+    os.rename('/Users/admin/PycharmProjects/pythonProject/hf.py', '/Users/admin/PycharmProjects/pythonProject/new_name.py ')
+    print('Your filename has changed ')
+
+
+
