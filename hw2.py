83e7d1370093811a20691a074c10f401a2db7f16
Anna Sivohina <38498089+mashaanya@users.noreply.github.com>
2020-09-30 23:56:41 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -16,11 +16,17 @@ def scale2(vec, c):
     return [vec[0] * c, vec[1] * c, vec[2] * c]
 
 
-def rotate2(vec, angle):
-    mat = [[math.cos(angle), 0, -math.sin(angle)], [0, 1, 0], [math.sin(angle), 0, math.cos(angle)]]
+def rotate2(vec, angle, axis):
+    if axis == 'Y':
+        mat = [[math.cos(angle), 0, -math.sin(angle)], [0, 1, 0], [math.sin(angle), 0, math.cos(angle)]]
+    if axis == 'X':
+        mat = [[1, 0, 0], [0, math.cos(angle), -math.sin(angle)], [0, math.sin(angle), math.cos(angle)]]
+    if axis == 'Z':
+        mat = [[math.cos(angle), -math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0], [0, 0, 1]]
     newvec = [vec[0] * mat[0][0] + vec[1] * mat[0][1] + vec[2] * mat[0][2],
               vec[0] * mat[1][0] + vec[1] * mat[1][1] + vec[2] * mat[1][2],
               vec[0] * mat[2][0] + vec[1] * mat[2][1] + vec[2] * mat[2][2]]
+
     return newvec
 
 
@@ -43,7 +49,9 @@ elif dimension == '3D':
     new = scale2(vec, constant)
     print('scaled', *new)
     angle = float(input()) * math.pi / 180
-    print('initial', *rotate2(vec, angle))
-    print('scaled', *rotate2(new, angle))
+    axis = input()
+    print('initial', *rotate2(vec, angle, axis))
+    print('scaled', *rotate2(new, angle, axis))
 else:
     print('please try againg and input demension')
+
